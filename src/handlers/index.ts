9ac7b19b850d7d6247b3d162c23c40e1a1e1ab1d
Pablo Andres Chacon <pabloandres120297@gmail.com>
2026-02-23 20:46:11 -0500

    add cors

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -12,23 +12,21 @@ export const createAccount = async (req: Request, res: Response) => {
   const userExists = await User.findOne({email});
   if (userExists) {
     const error = new Error('Ya existe un usuario con este correo, inice sesion');
-    res.status(409).json({error: error.message});
-    return;
+    return res.status(409).json({error: error.message});
   }
 
   const handle = slug(req.body.handle, '');
   const handleExists = await User.findOne({handle});
   if (handleExists) {
     const error = new Error('Ya existe un usuario con este nombre, inice sesion');
-    res.status(409).json({error: error.message});
-    return;
+    return res.status(409).json({error: error.message});
   }
 
   const user = new User(req.body);
   user.password = await hasPassword(password);
   user.handle = handle;
   await user.save();
-  res.send({msg: 'Registro creado correctamente'});
+  return res.send({msg: 'Registro creado correctamente'});
 }
 
 export const login = async (req: Request, res: Response) => {
@@ -41,16 +39,14 @@ export const login = async (req: Request, res: Response) => {
   const user = await User.findOne({email});
   if (!user) {
     const error = new Error('No existe un usuario con este correo, registrese');
-    res.status(404).json({error: error.message});
-    return;
+    return res.status(404).json({error: error.message});
   }
   const isPasswordCorrect = await checkPassword(password, user.password)
   if (!isPasswordCorrect) {
     const error = new Error('El password es incorrecto');
-    res.status(404).json({error: error.message});
-    return;
+    return res.status(404).json({error: error.message});
   }
-  res.send('Desde login');
+  return res.send('Desde login');
 }
 
 
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,15 @@
 import express from 'express'
 import 'dotenv/config'
+import cors from 'cors'
 
 import router from './router'
 import { connectDB } from './config/db';
+import { corsConfig } from './config/cors';
+connectDB();
 
 const app = express();
-
-connectDB();
+// Cors
+app.use(cors(corsConfig));
 
 // Leer datos de envio
 app.use(express.json());
